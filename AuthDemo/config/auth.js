7fead26b43eab188639ea3fcbdc50ce4ba770e35
Lohit Peesapati <psslohith@gmail.com>
2017-05-02 16:44:32 +0000

    modified url of auth.js

--- AuthDemo/config/auth.js
+++ AuthDemo/config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : 215642455602852, // your App ID
         'clientSecret'  : "285f5d07a57e2d5b94a18d16efcc11a4", // your App Secret
-        'callbackURL'   : 'https://webdeveloper-bootcamp-lohit-peesapati.c9users.io/auth/facebook/callback'
+        'callbackURL'   : 'https://whispering-brook-14323.herokuapp.com/auth/facebook/callback'
     }
 };
